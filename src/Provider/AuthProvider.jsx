b440cab5b5bd353c4127e52e5320970d3faa3cf0
Junaid Hasan <spiderss907@gmail.com>
2024-05-24 21:35:17 +0600

    authentication user problem solve

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,9 +1,9 @@
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
 import { createContext, useEffect, useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 
-export const AuthContext = createContext(null)
+export const AuthContext = createContext(null);
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
@@ -15,48 +15,57 @@ const AuthProvider = ({ children }) => {
     const createUser = (email, password) => {
         setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
-    }
+    };
 
     const signIn = (email, password) => {
         setLoading(true)
         return signInWithEmailAndPassword(auth, email, password)
-    }
+    };
 
     const googleSignIn = () => {
         setLoading(true)
         return signInWithPopup(auth, googleProvider);
-    }
+    };
 
     const logOut =()=>{
         setLoading(true)
         return signOut(auth)
-    }
+    };
     const updateUserProfile =(name, photo)=>{
         return updateProfile(auth.currentUser,{
             displayName: name, photoURL: photo
         })
-    }
-    useEffect(() => {
-       const unsubscribe = onAuthStateChanged(auth, currenUser => {
-            setUser(currenUser)
-            console.log('current user', currenUser);
-            // get and set token
-            if(currenUser){
-                axios.post('https://summer-camp-school-server-junaidhasan75.vercel.app/jwt', {email:currenUser.email})
-                .then(data =>{
-                    // console.log(data.data.token)
-                    localStorage.setItem('access-token', data.data.token)
-                    setLoading(false)
-                })
-            }
-            else{
-                localStorage.removeItem('access-token')
-            }
-        })
-        return ()=>{
-            return unsubscribe()
-        }
-    }, [])
+    };
+    // useEffect(() => {
+    //    const unsubscribe = onAuthStateChanged(auth, currenUser => {
+    //         setUser(currenUser)
+    //         console.log('current user', currenUser);
+    //         // get and set token
+    //         if(currenUser){
+    //             axios.post('https://summer-camp-school-server-junaidhasan75.vercel.app/jwt', {email:currenUser.email})
+    //             .then(data =>{
+    //                 // console.log(data.data.token)
+    //                 localStorage.setItem('access-token', data.data.token)
+    //             })
+    //             setLoading(false)
+    //         }
+    //         else{
+    //             localStorage.removeItem('access-token')
+    //         }
+    //     })
+    //     return ()=>{
+    //         return unsubscribe()
+    //     }
+    // }, [])
+    useEffect(()=>{
+        const unsubscribe =  onAuthStateChanged(auth, (currentUser)=>{
+              setUser(currentUser);
+              setLoading(false);
+          })
+          return ()=>{
+              return unsubscribe()
+          }
+      },[]);
 
     const authInfo = {
         user,
